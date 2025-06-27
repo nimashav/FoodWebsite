591b6c7d543b7f9e4060b636f47a7ec886983765
nimashav <nnimasha43@gmail.com>
2025-06-27 17:44:37 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "some dope pizza yo",
+    }),
+    React.createElement(Pizza, {
+      name: "Americano Pizza",
+      description: "French fries and hot dogs, wtf Italy",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian",
+      description: "Pineapple and ham, wtf AMerica",
+    }),
+    React.createElement(Pizza, {
+      name: "Chicken Pizza",
+      description: "Chicken nuggies on your pizza, wtf UK",
+    }),
+    React.createElement(Pizza, {
+      name: "Baked Potato Pizza",
+      description: "Unholy potato mash, wtf Minnesota",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
