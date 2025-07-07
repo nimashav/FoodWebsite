fc4df9c64e02d740279ba85e96d177d71e8e8a2d
nimashav <nnimasha43@gmail.com>
2025-07-07 20:52:54 +0530

    create past order page

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -4,6 +4,7 @@ import Header from '../Header'
 import PizzaOfTheDay from '../PizzaOfTheDay'
 import { useState } from 'react'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export const Route = createRootRoute({
   component: () => {
@@ -18,6 +19,7 @@ export const Route = createRootRoute({
           </div>
         </CartContext.Provider>
         <TanStackRouterDevtools />
+        <ReactQueryDevtools />
       </>
     )
   }
