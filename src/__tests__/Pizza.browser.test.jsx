25444d913537b3872aa39e3331563d2dbe50bd32
nimashav <nnimasha43@gmail.com>
2025-07-09 01:24:22 +0530

    finished testing

---
+++ src/__tests__/Pizza.browser.test.jsx
@@ -0,0 +1,18 @@
+import { render } from "@testing-library/react";
+import { expect, test } from "vitest";
+import Pizza from "../Pizza";
+
+
+test("alt text renders on image", async () => {
+  const name = "My favorite pizza";
+  const src = "https://picsum.photos/200";
+  const screen = render(
+    <Pizza name={name} image={src} description="cool browser stuff" />,
+  );
+
+  const img = await screen.getByRole("img");
+
+  await expect.element(img).toBeInTheDocument();
+  await expect.element(img).toBeInTheDocument("src", src);
+  await expect.element(img).toBeInTheDocument("alt", name);
+});
